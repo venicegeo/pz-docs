838e2f9592e31059ac2960c46ded8452d31ab0c2
Seth Baxter <seth@neuralux.com>
2016-04-12 13:40:24 -0400

    Archive will generate html for all documents

--- ci/archive.sh
+++ ci/archive.sh
@@ -6,6 +6,9 @@ popd > /dev/null
 
 source $root/ci/vars.sh
 
-python scripts/asciidoc-8.6.9/asciidoc.py -o out/piazza.html documents/piazza.txt
+for f in $(ls -1 $root/documents) ; do
+  base=$(basename $root/$f .txt)
+  python $root/scripts/asciidoc-8.6.9/asciidoc.py -o out/$base.html documents/$f
+done
 
 tar -czf $APP.$EXT -C $root out

4c7e4e4e22e835b035969e7c1504c85d09e4b125
Seth Baxter <seth@neuralux.com>
2016-04-12 11:53:39 -0400

    Modify pipeline scripts

--- ci/archive.sh
+++ ci/archive.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -ex
 
-#pushd `dirname $0`/.. > /dev/null
-#root=$(pwd -P)
-#popd > /dev/null
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
+popd > /dev/null
 
-#source $root/ci/vars.sh
-
-#tar -czf $APP.$EXT -C $root public
+source $root/ci/vars.sh
 
 python scripts/asciidoc-8.6.9/asciidoc.py -o out/piazza.html documents/piazza.txt
 
-#python scripts/asciidoc-8.6.9/asciidoc.py -o out/piazza.html piazza.txt
+tar -czf $APP.$EXT -C $root out

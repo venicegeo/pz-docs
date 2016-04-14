a203e02350ca28cd5421b4dca6600577e4b9b8f1
mpg <mpg@flaxen.com>
2016-04-14 15:46:15 -0400

    from last night - scaffolding complete, wiki contents dumped in (still raw)

--- ci/archive.sh
+++ ci/archive.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 pushd `dirname $0`/.. > /dev/null
 root=$(pwd -P)
@@ -6,9 +6,30 @@ popd > /dev/null
 
 source $root/ci/vars.sh
 
-for f in $(ls -1 $root/documents | grep .txt$) ; do
-  base=$(basename $root/$f .txt)
-  python $root/scripts/asciidoc-8.6.9/asciidoc.py -o $root/out/$base.html $root/documents/$f
-done
+function doit {
+    indir=$1
+    outdir=$2
+    
+    cmd="python $root/scripts/asciidoc-8.6.9/asciidoc.py"
+    $cmd -o $outdir/index.html $indir/index.txt 2> stderr.tmp
+}
+
+rm -f out/*.html out/*/*.html
+rm -f stderr.tmp
+
+ins="$root/documents"
+outs="$root/out"
+
+doit $ins $outs
+doit $ins/userguide $outs/userguide
+doit $ins/devguide $outs/devguide
+doit $ins/devopsguide $outs/devopsguide
+
+# treat asciidoc warnings as errors
+if [ -s stderr.tmp ] ;
+then
+  cat stderr.tmp
+  exit 1
+fi
 
 tar -czf $APP.$EXT -C $root out

27a1edb23c53e74da2b0b11402628f800453b600
mpg <mpg@flaxen.com>
2016-04-25 23:04:21 -0400

    starting to fill out tutorial

--- documents/userguide/scripts/hi.sh
+++ documents/userguide/scripts/hi.sh
@@ -2,11 +2,16 @@
 
 source ./documents/userguide/scripts/vars.sh
 
-# tag::public[] 
+# tag::public[]
+# exit the script if any error 
 set -e
 
+# ping the gateway, sending the returned string to a file
+# "-S -s" means show any errors but don't show the progress meter
 curl -S -s -XGET -o response.txt http://pz-gateway.$domain/health
 
+# verify the response was "OK"
+# (because of set -e above, script exits with an error if the match fails)
 grep -q OK response.txt
 # end::public[] 
 

92c24019230510e593113bbd1e6359d5743aa9de
mpg <mpg@flaxen.com>
2016-04-27 15:52:01 -0400

    section numbering; renamed files to match sections; cleaned up hi.sh

--- documents/userguide/scripts/hi.sh
+++ documents/userguide/scripts/hi.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
-source ./documents/userguide/scripts/vars.sh
+printenv DOMAIN > /dev/null
 
 # tag::public[]
-# exit the script if any error 
-set -e
-
 # ping the gateway, sending the returned string to a file
-# "-S -s" means show any errors but don't show the progress meter
-curl -S -s -XGET -o response.txt http://pz-gateway.$domain/health
+curl -S -s -XGET -o response.txt http://pz-gateway.$DOMAIN/health
 
 # verify the response was "OK"
-# (because of set -e above, script exits with an error if the match fails)
 grep -q OK response.txt
 # end::public[] 
 
 rm -f response.txt
+
+echo Pass.
--- documents/userguide/scripts/vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ "$PCF_DOMAIN" = "" ] ; then
-    PCF_DOMAIN=stage.geointservices.io
-fi
-domain=$PCF_DOMAIN

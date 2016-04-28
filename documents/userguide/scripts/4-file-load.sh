4284b01bb08152d3bd96291da77b3bd11b2273ad
mpg <mpg@flaxen.com>
2016-04-28 17:50:02 -0400

    load & access examples

---
+++ documents/userguide/scripts/4-file-load.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -ex
+
+printenv DOMAIN > /dev/null
+
+# tag::public[]
+data="{
+    \"type\": \"ingest\",
+    \"host\": true,
+    \"data\": {
+        \"dataType\": {
+            \"type\": \"raster\"
+        },
+        \"metadata\": {
+            \"name\": \"terrametrics\",
+            \"description\": \"geotiff_test\"
+        }
+    }
+}"
+
+curl -S -s -X POST \
+    -w "%{http_code}" \
+    -o response.txt \
+    -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" \
+    -F "data=$data" \
+    -F "file=@./out/userguide/scripts/terrametrics.tif" "https://pz-gateway.$DOMAIN/data/file" > status.txt
+
+# verify all worked successfully
+grep -q 200 status.txt
+grep -q jobId response.txt
+
+# print out the JobId
+grep -o '"jobId":".*"' response.txt | cut -d \" -f 4
+# end::public[] 
+
+rm -f response.txt status.txt

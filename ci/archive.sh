5fd8066fa66babe64cb89b4aa142f58d5094f37b
mpg <mpg@flaxen.com>
2016-05-04 15:34:59 -0400

    move testing into operate function; add presentation PDFs

--- ci/archive.sh
+++ ci/archive.sh
@@ -38,6 +38,32 @@ function doit {
     cp -R $indir/scripts $outdir
 }
 
+
+function run_tests {
+    # verify the example scripts
+    echo Checking examples.
+
+    echo "Checking section 3 examples"
+    #$root/documents/userguide/scripts/3-hello.sh
+    #$root/documents/userguide/scripts/3-hello-full.sh
+
+    echo "Checking section 4 examples"
+    #jobid=`$root/documents/userguide/scripts/4-file-load.sh`
+    #dataid=`$root/documents/userguide/scripts/4-file-job.sh $jobid`
+    #$root/documents/userguide/scripts/4-file-info.sh $dataid
+    #$root/documents/userguide/scripts/4-file-download.sh $dataid
+    #$root/documents/userguide/scripts/4-file-wms.sh $dataid
+
+    echo "Checking section 5 examples"
+
+    echo "Checking section 6 examples"
+
+    echo "Checking section 7 examples"
+
+    echo Examples checked.
+}
+
+
 rm -fr $root/out/*
 
 ins="$root/documents"
@@ -48,27 +74,10 @@ doit $ins/userguide   $outs/userguide
 doit $ins/devguide    $outs/devguide
 doit $ins/devopsguide $outs/devopsguide
 
-# verify the example scripts
-echo Checking examples.
+mkdir $outs/presentations
+cp -f $ins/presentations/*.pdf $outs/presentations/
 
-echo "Checking section 3 examples"
-#$root/documents/userguide/scripts/3-hello.sh
-#$root/documents/userguide/scripts/3-hello-full.sh
-
-echo "Checking section 4 examples"
-#jobid=`$root/documents/userguide/scripts/4-file-load.sh`
-#dataid=`$root/documents/userguide/scripts/4-file-job.sh $jobid`
-#$root/documents/userguide/scripts/4-file-info.sh $dataid
-#$root/documents/userguide/scripts/4-file-download.sh $dataid
-#$root/documents/userguide/scripts/4-file-wms.sh $dataid
-
-echo "Checking section 5 examples"
-
-echo "Checking section 6 examples"
-
-echo "Checking section 7 examples"
-
-echo Examples checked.
+run_tests
 
 echo Done.
 

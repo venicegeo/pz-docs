364f5301a2a037517cdf2b3412095bfcb39aa90e
Stefan VanBuren <svanburen@radiantblue.com>
2016-05-24 15:08:52 -0400

    Fix file load example

--- documents/userguide/scripts/4-file-load.sh
+++ documents/userguide/scripts/4-file-load.sh
@@ -22,14 +22,14 @@ curl -S -s -X POST \
     -o response.txt \
     -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" \
     -F "data=$data" \
-    -F "file=@./out/userguide/scripts/terrametrics.tif" "https://pz-gateway.$DOMAIN/data/file" > status.txt
+    -F "file=@./terrametrics.tif" "https://pz-gateway.$DOMAIN/data/file" > status.txt
 
 # verify all worked successfully
 grep -q 200 status.txt
 grep -q jobId response.txt
 
-# print out the JobId
-grep -o '"jobId":".*"' response.txt | cut -d \" -f 4
+# print out the jobId
+grep -E -o '"jobId"\s?:\s?".*"' response.txt | cut -d \" -f 4
 # end::public[] 
 
 rm -f response.txt status.txt

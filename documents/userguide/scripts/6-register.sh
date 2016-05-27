de2ecd96ab20c4d8e9fe6744ccb5482623103223
Stefan VanBuren <svanburen@radiantblue.com>
2016-05-27 15:19:20 -0400

    Update 6-register.sh to use proper endpoint

--- documents/userguide/scripts/6-register.sh
+++ documents/userguide/scripts/6-register.sh
@@ -21,11 +21,11 @@ curl -X POST -S -s \
     -H 'Content-Type: application/json' \
     -o response.txt \
     -d "$service" \
-    "https://pz-gateway.$DOMAIN/v1/hello" > status.txt
+    "https://pz-gateway.$DOMAIN/service" > status.txt
 
 grep -q 200 status.txt
 # print serviceId
 grep -E -o '"serviceId"\s?:\s?".*"' response.txt | cut -d \" -f 4
 # end::public[]
 
-#rm -f response.txt status.txt
+rm -f response.txt status.txt

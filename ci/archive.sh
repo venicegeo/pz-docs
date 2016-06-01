e227095608d7833450125bc3d98b7950b2e24bbe
Stefan VanBuren <svanburen@radiantblue.com>
2016-06-01 14:38:15 -0400

    Shellchecked. Also updated
    Non-hosted part of chapter 4 doesn't seem to work -- probably because the non-hosted file doesn't actually exist right now

--- ci/archive.sh
+++ ci/archive.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-set -e
+#set -e
 
-pushd `dirname $0`/.. > /dev/null
+ins="$root/documents"
+outs="$root/out"
+scripts="$root/documents/userguide/scripts"
+
+[[ -f "$scripts/setup.sh" ]] && . "$scripts/setup.sh"
+
+pushd "$(dirname "$0")/.." > /dev/null
 root=$(pwd -P)
 popd > /dev/null
 
 hash asciidoctor >/dev/null 2>&1 || gem install asciidoctor
 hash asciidoctor-pdf >/dev/null 2>&1 || gem install --pre asciidoctor-pdf
 
-source $root/ci/vars.sh
+source "$root/ci/vars.sh"
 
 function doit {
     indir=$1
     outdir=$2
 
-    aaa=`dirname $indir/index.txt`
-    bbb=`basename $aaa`
+    aaa=$(dirname "$indir/index.txt")
+    bbb=$(basename "$aaa")
     echo "Processing: $bbb/index.txt"
 
     # txt -> html
-    asciidoctor -o $outdir/index.html $indir/index.txt  &> errs.tmp
+    asciidoctor -o "$outdir/index.html" "$indir/index.txt"  &> errs.tmp
     if [[ -s errs.tmp ]] ; then
         cat errs.tmp
         exit 1
     fi
 
     # txt -> pdf
-    asciidoctor -r asciidoctor-pdf -b pdf -o $outdir/index.pdf $indir/index.txt  &> errs.tmp
+    asciidoctor -r asciidoctor-pdf -b pdf -o "$outdir/index.pdf" "$indir/index.txt"  &> errs.tmp
     if [[ -s errs.tmp ]] ; then
         cat errs.tmp
         exit 1
@@ -36,65 +42,75 @@ function doit {
     [[ -s "errs.tmp" ]] || rm "errs.tmp"
 
     # copy images directory to out dir
-    cp -R $indir/images $outdir
+    cp -R "$indir/images" "$outdir"
 
     # copy scripts directory to out dir
-    cp -R $indir/scripts $outdir
+    cp -R "$indir/scripts" "$outdir"
 }
 
 
 function run_tests {
     # verify the example scripts
-    echo Checking examples.
+    echo
+    echo "Checking examples."
 
+    echo
     echo "Checking section 3 examples"
-    echo "Checking 3-hello.sh"
-    $root/documents/userguide/scripts/3-hello.sh
-    echo "Checking 3-hello-full.sh"
-    $root/documents/userguide/scripts/3-hello-full.sh
+    echo "- Checking 3-hello.sh"
+    sh "$scripts/3-hello.sh"
+    echo "- Checking 3-hello-full.sh"
+    sh "$scripts/3-hello-full.sh"
 
+    echo
     echo "Checking section 4 examples"
-    cp $root/documents/userguide/scripts/terrametrics.tif $root
-    echo "Checking 4-hosted-load.sh"
-    jobid=`$root/documents/userguide/scripts/4-hosted-load.sh`
-    echo "Checking 4-job.sh"
-    dataid=`$root/documents/userguide/scripts/4-job.sh $jobid`
-    echo "Checking 4-hosted-download.sh"
-    $root/documents/userguide/scripts/4-hosted-download.sh $dataid
-    # echo "Checking 4-nonhosted-load.sh"
-    # jobid=`$root/documents/userguide/scripts/4-nonhosted-load.sh`
-    # echo "Checking 4-job.sh"
-    # dataid=`$root/documents/userguide/scripts/4-job.sh $jobid`
-    # echo "Checking 4-nonhosted-wms.sh"
-    # $root/documents/userguide/scripts/4-nonhosted-wms.sh $dataid
-    rm $root/terrametrics.tif
+    cp "$scripts/terrametrics.tif" "$root"
+    echo "- Checking 4-hosted-load.sh"
+    jobid=$(sh "$scripts/4-hosted-load.sh")
+    echo "- Checking 4-job.sh"
+    dataid=$(sh "$scripts/4-job.sh" "$jobid")
+    echo "- Checking 4-hosted-download.sh"
+    sh "$scripts/4-hosted-download.sh" "$dataid"
+    rm "$root/terrametrics.tif"
 
-    echo "Checking section 5 examples"
+    # echo "- Checking 4-nonhosted-load.sh"
+    # jobid=`$scripts/4-nonhosted-load.sh`
+    # echo "- Checking 4-job.sh"
+    # dataid=`$scripts/4-job.sh $jobid`
+    # echo "- Checking 4-nonhosted-wms.sh"
+    # jobid=`$scripts/4-nonhosted-wms.sh $dataid`
+    # # check to make sure our wms gets set up
+    # $scripts/4-job.sh $jobid
 
-    echo "Checking section 6 examples"
+    # echo
+    # echo "Checking section 5 examples"
 
-    echo "Checking section 7 examples"
+    # echo
+    # echo "Checking section 6 examples"
 
-    echo Examples checked.
+    # echo
+    # echo "Checking section 7 examples"
+
+    # echo
+    # echo "Checking section 8 examples"
+
+    # echo
+    echo "Examples checked."
 }
 
-ins="$root/documents"
-outs="$root/out"
+[[ -d "$outs" ]] && rm -rf "$outs"
 
-[[ -d "$outs" ]] && rm -rf $outs
+mkdir "$outs"
 
-mkdir $outs
+doit "$ins" "$outs"
+doit "$ins/userguide"   "$outs/userguide"
+doit "$ins/devguide"    "$outs/devguide"
+doit "$ins/devopsguide" "$outs/devopsguide"
 
-doit $ins $outs
-doit $ins/userguide   $outs/userguide
-doit $ins/devguide    $outs/devguide
-doit $ins/devopsguide $outs/devopsguide
-
-mkdir $outs/presentations
-cp -f $ins/presentations/*.pdf $outs/presentations/
+mkdir "$outs/presentations"
+cp -f "$ins/presentations/*.pdf" "$outs/presentations/"
 
 run_tests
 
 echo Done.
 
-tar -czf $APP.$EXT -C $root out
+tar -czf "$APP.$EXT" -C "$root" out

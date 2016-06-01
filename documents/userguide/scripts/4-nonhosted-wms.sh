990998b3c00307c9fdc717e98917291fff0e2e10
Stefan VanBuren <svanburen@radiantblue.com>
2016-06-01 09:48:59 -0400

    Add 4-nonhosted-wms.sh

---
+++ documents/userguide/scripts/4-nonhosted-wms.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+printenv DOMAIN > /dev/null
+
+# tag::public[]
+id=$1
+
+data="{
+    \"type\": \"access\",
+    \"dataId\": \"$id\",
+    \"deploymentType\": \"geoserver\"
+}"
+
+curl -S -s -X POST \
+    -u $PZUSER:$PZPASS \
+    -H 'Content-Type: application/json' \
+    -d "$data" \
+    -w "%{http_code}" \
+    -o response.txt \
+    "https://pz-gateway.$DOMAIN/deployment" > status.txt
+
+# verify all worked successfully
+grep -q 200 status.txt
+
+# print out the jobId
+grep -E -o '"jobId"\s?:\s?".*"' response.txt | cut -d \" -f 4
+# end::public[]
+
+#rm -f response.txt status.txt

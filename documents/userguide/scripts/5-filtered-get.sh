03ef402b9bc246f36ee58fbb2645bac0358f6a5c
Stefan VanBuren <svanburen@radiantblue.com>
2016-06-03 10:28:32 -0400

    Update scripts
    More detail if 3-hello-full.sh fails, change the idea behind 5-filtered
    get in that it'll print out the metadata requested, and add more detail
    to 8-endtoend.sh (still not done though).

--- documents/userguide/scripts/5-filtered-get.sh
+++ documents/userguide/scripts/5-filtered-get.sh
@@ -3,7 +3,6 @@ set -e
 
 # tag::public[]
 term=$1
-id=$2
 
 curl -X GET -S -s \
     -w "%{http_code}" \
@@ -12,7 +11,7 @@ curl -X GET -S -s \
     "https://pz-gateway.$DOMAIN/data?keyword=$term&page=0&per_page=100" > status.txt
 
 grep -q 200 status.txt || { cat response.txt; exit 1; }
-grep -E -o "$id" response.txt
+cat response.txt
 # end::public[]
 
 rm -f response.txt status.txt
--- documents/userguide/scripts/8-endtoend.sh
+++ documents/userguide/scripts/8-endtoend.sh
@@ -13,6 +13,21 @@ service='{
     }
 }'
 
+payload='{
+    "source": {
+        "domain": "s3.amazonaws.com",
+        "bucketName": "pz-svcs-prevgen",
+        "fileName": "NASA-GDEM-10km-colorized.tif"
+    },
+    "function": "crop",
+    "bounds": {
+        "minx": -140.00,
+        "miny": 10.00,
+        "maxx": -60.00,
+        "maxy": 70.00
+    }
+}'
+
 curl -X POST -S -s \
     -u "$PZUSER":"$PZPASS" \
     -w "%{http_code}" \
@@ -24,6 +39,14 @@ curl -X POST -S -s \
 grep -q 200 status.txt || { cat response.txt; exit 1; }
 # print serviceId
 grep -E -o '"serviceId"\s?:\s?".*"' response.txt | cut -d \" -f 4
+
+curl -X POST -S -s \
+    -u "$PZUSER":"$PZPASS" \
+    -w "%{http_code}" \
+    -H 'Content-Type: application/json' \
+    -o response.txt \
+    -d "$payload" \
+    "https://pz-svcs-prevgen.$DOMAIN/crop" > status.txt
 # end::public[]
 
 rm -f response.txt status.txt

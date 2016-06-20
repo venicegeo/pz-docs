0c5607e6789fd71934185e39c086cdc1694ffdbb
Stefan VanBuren <svanburen@radiantblue.com>
2016-06-20 15:20:17 -0400

    Remove and rename redundant scripts

--- documents/userguide/scripts/job-info.sh
+++ documents/userguide/scripts/job-info.sh
@@ -7,12 +7,14 @@ jobId=$1
 curl -X GET -S -s \
     -u "$PZUSER":"$PZPASS" \
     -w "%{http_code}" \
-    -H 'Content-Type: application/json' \
     -o response.txt \
     "https://pz-gateway.$DOMAIN/job/$jobId" > status.txt
 
+# verify all worked successfully
 grep -q 200 status.txt || { cat response.txt; exit 1; }
-# print dataId
+grep -E -q '"status"\s?:\s?"Success"' response.txt || { cat response.txt; exit 1; }
+
+# print the data's resource id
 grep -E -o '"dataId"\s?:\s?".*"' response.txt | cut -d \" -f 4
 
 # end::public[]

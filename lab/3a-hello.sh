156a141a2c29a042f6ecadeb1fdedd92c4ef6d55
mpg <mpg@flaxen.com>
2016-09-06 11:11:55 -0400

    all OBE

--- lab/3a-hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -u "$PZUSER":"$PZPASS" -X GET "http://pz-gateway.$DOMAIN/health"
--- lab/3b-hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-. setup.sh
-
-$curl -X GET $url/health
-
-cat response.txt
-echo
